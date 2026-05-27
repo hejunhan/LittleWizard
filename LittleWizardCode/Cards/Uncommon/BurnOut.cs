95e905a5a715edea80557c7fbe1423883d4f6f81
青白影 <376565306@qq.com>
2026-05-27 17:52:17 +0800

    BurnOut.cs增强

--- LittleWizardCode/Cards/Uncommon/BurnOut.cs
+++ LittleWizardCode/Cards/Uncommon/BurnOut.cs
@@ -33,7 +33,6 @@ public class BurnOut()
     {
         await CommonActions.CardAttack(this, cardPlay).Execute(choiceContext);
         ArgumentNullException.ThrowIfNull(cardPlay.Target);
-        await PowerCmd.Remove<FireElement>(cardPlay.Target);
         await AnimationHelper.TriggerCastAnimationOwner(this);
     }
 
