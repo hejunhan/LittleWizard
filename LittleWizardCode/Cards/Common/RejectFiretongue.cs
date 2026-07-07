9717f774dba6bca0a08839c17bcf1ac497ed761a
hyjchm <18321900580@163.com>
2026-07-08 02:11:06 +0800

    Remove element tag from Reject Firetongue

--- LittleWizardCode/Cards/Common/RejectFiretongue.cs
+++ LittleWizardCode/Cards/Common/RejectFiretongue.cs
@@ -1,7 +1,6 @@
 using BaseLib.Extensions;
 using BaseLib.Utils;
 using LittleWizard.LittleWizardCode.Api.Cards;
-using LittleWizard.LittleWizardCode.Api.Extensions;
 using LittleWizard.LittleWizardCode.Powers.Elements;
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
@@ -17,8 +16,6 @@ namespace LittleWizard.LittleWizardCode.Cards.Common;
 public class RejectFiretongue()
     : LittleWizardCard(0, CardType.Skill, CardRarity.Common, TargetType.Self)
 {
-    protected override HashSet<CardTag> CanonicalTags => [CardTagExtensions.LittleWizardElement];
-
     protected override IEnumerable<DynamicVar> CanonicalVars => [new BlockVar(3, ValueProp.Move)];
 
     protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
