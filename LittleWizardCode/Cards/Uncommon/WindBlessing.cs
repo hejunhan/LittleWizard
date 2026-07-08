5a3c9777231f33ddd740df441f649dca657fa7fb
hyjchm <18321900580@163.com>
2026-07-08 15:22:54 +0800

    修改描述，动态变量命名

--- LittleWizardCode/Cards/Uncommon/WindBlessing.cs
+++ LittleWizardCode/Cards/Uncommon/WindBlessing.cs
@@ -2,7 +2,6 @@ using BaseLib.Extensions;
 using BaseLib.Utils;
 using LittleWizard.LittleWizardCode.Api.Cards;
 using LittleWizard.LittleWizardCode.Api.Extensions;
-using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
@@ -17,13 +16,7 @@ public class WindBlessing()
 
     protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
     {
-        await PowerCmd.Apply<DexterityPower>(
-            choiceContext,
-            Owner.Creature,
-            DynamicVars.Power<DexterityPower>().BaseValue,
-            Owner.Creature,
-            this
-        );
+        await CommonActions.Apply<DexterityPower>(choiceContext, this, cardPlay);
     }
 
     protected override void OnUpgrade()
